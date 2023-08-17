145b0cb12a57cb0114f69765ca1b83cf8bfb62ab
Kaleidoscopee <133397937+Kaleidoscopee@users.noreply.github.com>
2023-08-17 11:39:31 -0400

    put magic8ball and tip seperate file

--- server.js
+++ server.js
@@ -28,46 +28,6 @@ app.get('/hello/:firstname/:lastname', (req, res)=> {
     res.send(`hello ${req.params.firstname} ${req.params.lastname}`)
 })
 
-// app.get('/tip/:total/:tipPercentage', (req, res)=> {
-//     const tipAmount = req.params.total * req.params.tipPercentage / 100;
-//     res.send(`Tip is $${tipAmount}.`);
-// })
-
-magic8BallResponse = [
-    "It is certain", 
-    "It is decidedly so", 
-    "Without a doubt", 
-    "Yes definitely",
-    "You may rely on it", 
-    "As I see it yes", 
-    "Most likely", 
-    "Outlook good",
-    "Yes", 
-    "Signs point to yes", 
-    "Reply hazy try again", 
-    "Ask again later",
-    "Better not tell you now", 
-    "Cannot predict now", 
-    "Concentrate and ask again",
-    "Don't count on it", 
-    "My reply is no", 
-    "My sources say no",
-    "Outlook not so good", 
-    "Very doubtful"
-];
-
-
-app.get('/magic/:question', (req, res)=> {
-    const question = req.params.question;
-    const response = magic8BallResponse[Math.floor(Math.random() * magic8BallResponse.length)];
-
-    res.send(`
-        <h1>Your Question: ${question}</h1>
-        <h1>Magic 8 Ball Response: ${response}</h1>
-    `)
-});
-
-
 app.get('/:id', (req, res)=> {
     //res.send(plants[0])
     if(plants[req.params.id]){
@@ -79,7 +39,7 @@ app.get('/:id', (req, res)=> {
 
 
 
-
+// finds port to render stuff on screen
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`)
 });
